1c14f3b80d845d5d9e8fc5f26892459387ba894e
6manansalaT <trinity.joy.manansala@gmail.com>
2026-03-05 14:45:39 +0800

    first push, react setup

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import table from './table.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={table}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
