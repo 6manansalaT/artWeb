4d12a93efb408a51a289d1b0c4f7b302a599b25a
6manansalaT <trinity.joy.manansala@gmail.com>
2026-03-06 18:18:51 +0800

    text scroll

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import table from './table.png'
+import all from './dawholething.png';
+import Typewriter from './Typewriter';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const script = ['Welcome to quixzie\'s shop!', 'Take a look around...'];
+
+  const [currentInd, setCurrentInd] = useState(0);
+
+  const handleKeyPress = (event) => {
+    if(event.key === 'Enter') {
+      setCurrentInd(prevInd => prevInd + 1)
+      // move to next item in script
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={table}/>
+        <img className='All-img' src={all} alt='all'/>
+        <h1 className='Txt-box'>
+          <Typewriter text={script[currentInd]} delay={50} onKeyPress={handleKeyPress} />
+        </h1>
       </header>
     </div>
   );
